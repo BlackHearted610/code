b7ee647143392d760e98855d665394831535ef74
URMOM <116090729+BlackHearted610@users.noreply.github.com>
2022-12-14 16:40:35 +0000

    legends never die, accept me

--- End-credits.py
+++
@@ -1,4 +0,0 @@
-print ('Hello, this is just a side note. Thank you again for reading, this really took a long time to make and this has been an awsome project that has deffinetly pushed my limits, and my knowlege of coding.This last part is made from python, which is different then what the rest of the page is made out of.\n\nI feel that this was so interesting to write more than 200 lines of code just for a school project.')
-
-print('Thank you for being so kind and encouraging, by lengthening the due date, and letting us choose the option for paper instead of just saying "Here is a paper, now write".\n\nthank you in all truth for this, signing off\n\n-Dean')
-
